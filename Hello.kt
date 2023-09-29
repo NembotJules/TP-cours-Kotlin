2cf97098d0d2c8707b34be3d3f38ece9942910a7
NembotJules <nembotjulesgithub@gmail.com>
2023-09-29 05:45:44 +0200

    Explore default values and compact functions

--- Hello.kt
+++ Hello.kt
@@ -41,29 +41,85 @@ fun randomDay() : String {
 
 
 // Use a when expression
-fun fishFood (day : String) : String {
-    var food = ""
-    when (day) {
-        "Monday" ->  "flakes"
-        "Tuesday" -> "pellets"
-        "Wednesday" -> "redworms"
-        "Thursday" -> "granules"
-        "Friday" -> "mosquitoes"
-        "Saturday" -> "lettuce"
-        "Sunday" -> "plankton"
-        else -> "nothing"
-    }
-    return food
+// fun fishFood (day : String) : String {
+//     var food = ""
+//     when (day) {
+//         "Monday" ->  "flakes"
+//         "Tuesday" -> "pellets"
+//         "Wednesday" -> "redworms"
+//         "Thursday" -> "granules"
+//         "Friday" -> "mosquitoes"
+//         "Saturday" -> "lettuce"
+//         "Sunday" -> "plankton"
+//         else -> "nothing"
+//     }
+//     return food
+// }
+
+// fun feedTheFish() {
+//     val day = randomDay()
+//     val food = fishFood(day)
+
+//     println ("Today is $day and the fish eat $food")
+//     //=>Today is Thursday and the fish eat granules
+// }
+
+// fun main(args: Array<String>) {
+//     feedTheFish()
+// }
+
+fun swim(speed: String = "fast") {
+   println("swimming $speed")
 }
+
+fun main() {
+    swim()
+    swim("slow")   // positional argument
+    swim(speed="turtle-like")   // named parameter
+
+    // swimming fast
+    // swimming slow
+    // swimming turtle-like
+}
+
+//If no default is specified for a parameter, the corresponding argument must always be passed.
+
+// fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+//     return when {
+//         temperature > 30 -> true
+//         dirty > 30 -> true
+//         day == "Sunday" ->  true
+//         else -> false
+//     }
+// }
 
 fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
-
     println ("Today is $day and the fish eat $food")
-    //=>Today is Thursday and the fish eat granules
+    println("Change water: ${shouldChangeWater(day)}")
+}
+//Today is Thursday and the fish eat granules
+//Change water: false
+
+//-----Compact functions--------//
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
+
+//updating shouldChangeWater()
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else  -> false
+    }
 }
 
-fun main(args: Array<String>) {
-    feedTheFish()
-}
+//les valeurs par défaut peuvent aussi etre des fonctions
+//exemple
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = getDirtySensorReading()): Boolean {}
+
