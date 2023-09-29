6f505868d1474e3a1b77576bdbd4a21f4bce8d1d
NembotJules <nembotjulesgithub@gmail.com>
2023-09-29 06:01:11 +0200

    Get started with filters

--- Hello.kt
+++ Hello.kt
@@ -123,3 +123,68 @@ fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boo
 //exemple
 fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = getDirtySensorReading()): Boolean {}
 
+//Get started with filters...
+val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+fun main() {
+    println( decorations.filter {it[0] == 'p'})
+    // eager, creates a new list
+    val eager = decorations.filter { it [0] == 'p' }
+    println("eager: $eager")
+     // lazy, will wait until asked to evaluate
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
+     // force evaluation of the lazy list
+    val newList = filtered.toList()
+    println("new list: $newList")
+     val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+
+
+    //  lazy: kotlin.sequences.TransformingSequence@5ba23b66
+    //     -----
+    //     access: rock
+    //     first: rock
+    //     -----
+    //     access: rock
+    //     access: pagoda
+    //     access: plastic plant
+    //     access: alligator
+    //     access: flowerpot
+    //     all: [rock, pagoda, plastic plant, alligator, flowerpot]
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+        println("access: $it")
+        it
+    }
+    println("-----")
+    println("filtered: ${lazyMap2.toList()}")
+
+//     ⇒
+// -----
+// access: pagoda
+// access: plastic plant
+// filtered: [pagoda, plastic plant]
+
+val mysports = listOf("basketball", "fishing", "running")
+val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+val mylist = listOf(mysports, myplayers, mycities)     // list of lists
+println("-----")
+println("Flat: ${mylist.flatten()}")
+
+// ⇒
+// -----
+// Flat: [basketball, fishing, running, LeBron James, Ernest Hemingway, Usain Bolt, Los Angeles, Chicago, Jamaica]
+
+}
+//⇒ [pagoda, plastic plant]
+//=> filtered: kotlin.sequences.FilteringSequence@386cc1c4
+//=> new list: [pagoda, plastic plant]
+
