4fa1d24c671fec41f9223a73344fedc2a501cd8e
NembotJules <nembotjulesgithub@gmail.com>
2023-09-29 05:00:26 +0200

    Create a main function

---
+++ tpFunction.kt
@@ -0,0 +1,10 @@
+//La fonction main avec les arguments...
+// fun main(args:Array<String>) {
+//     println("Hello, World")
+// }
+//=> Hello, World
+
+fun main(args: Array<String>) {
+    println("Hello, ${args[0]}")
+}
+//Hello Kotlin
