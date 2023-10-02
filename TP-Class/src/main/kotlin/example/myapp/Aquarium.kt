ec6a670ae735ad1cba7c857fc37ba08fd4346379
NembotJules <nembotjulesgithub@gmail.com>
2023-10-02 04:39:14 +0200

    "Created the printSize method"

--- TP-Class/src/main/kotlin/example/myapp/Aquarium.kt
+++ TP-Class/src/main/kotlin/example/myapp/Aquarium.kt
@@ -6,5 +6,11 @@ class Aquarium {
     var width: Int = 20
     var height:Int = 40
     var length: Int = 100
+
+    fun printSize() {
+        println("Width: $width cm " +
+                "Length: $length cm  "   +
+                "height:  $height cm")
+    }
 }
 //You can access properties directly, for example, myAquarium.length
