5653f863de508e95db50819c50d9533f2d3cb93f
NembotJules <nembotjulesgithub@gmail.com>
2023-10-03 05:07:15 +0200

    "3-Add class constructors"

--- TP-Class/src/main/kotlin/Main.kt
+++ TP-Class/src/main/kotlin/Main.kt
@@ -4,11 +4,22 @@
 import example.myapp.Aquarium
 
 fun buildAquarium() {
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
-    //Let set the height to 60 and print the changed dimension properties
-    myAquarium.height = 60
-    myAquarium.printSize()
+   val aquarium1 = Aquarium()
+    aquarium1.printSize()
+    //default height and length
+    val aquarium2 = Aquarium(width = 25)
+    aquarium2.printSize()
+    //default width
+    val aquarium3 = Aquarium(height = 35, length = 110)
+    aquarium3.printSize()
+    //everything custom
+    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+    aquarium4.printSize()
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    aquarium6.volume = 70
+    aquarium6.printSize()
+    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
 }
 
 fun main() {
--- TP-Class/src/main/kotlin/example/myapp/Aquarium.kt
+++ TP-Class/src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,16 +1,32 @@
 // TP CLASSES ET OBJETS...
+// SAAH NEMBOT JULES CLINTON --->  19GOO503
 
 package example.myapp
 
-class Aquarium {
-    var width: Int = 20
-    var height:Int = 40
-    var length: Int = 100
+class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40){
+
+    constructor(numberOfFish: Int) : this() {
+        // 2000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
+
+    var volume: Int
+        get() = width * height * length / 1000 // 1000 cm^3 = 1 liter
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
+    init {
+        println("Aquarium initialization")
+    }
+
 
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm  "   +
                 "height:  $height cm")
+        println("Volume:  $volume liters")
     }
 }
 //You can access properties directly, for example, myAquarium.length
