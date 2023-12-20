e131fa4b89f920a002b232341f5f371444d379d3
NembotJules <juniorbertrand761@gmail.com>
2023-12-20 11:35:27 +0100

    Final Project

---
+++ TheNewsApp/app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.thenewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "a02a10c5dc8248d0a9842b29e95f5030"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+
+    }
+}
